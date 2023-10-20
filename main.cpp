2e04626a873b1b5654add1051c2a48f3938b8d0d
ADMIN <ADMIN@ACHMED-BINTANG>
2023-10-20 11:03:06 +0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{	
+	//Daftar Menu
+	cout << setw(30) << "Daftar Menu" << endl;
+
+	cout << char(214);
+	for (int x = 0; x < 23; x++) {
+		cout << char(196);
+	}
+	cout << char(210);
+	for (int x = 0; x < 23; x++) {
+		cout << char(196);
+	}
+	cout << char(183) << endl;
+
+	cout << char(186) << "\t Makanan\t" << char(186) << "\t Minuman\t" << char(186) << endl;
+
+	cout << char(204);
+	for (int x = 0; x < 23; x++) {
+		cout << char(205);
+	}
+	cout << char(206);
+	for (int x = 0; x < 23; x++) {
+		cout << char(205);
+	}
+	cout << char(185) << endl;
+
+	cout << char(186) << "1. Magelangan Rp.15.000" << char(186) << "1. Magelangan Rp.15.000" << char(186) << endl;
+	cout << char(186) << "2. Magelangan Rp.15.000" << char(186) << "2. Magelangan Rp.15.000" << char(186) << endl;
+	cout << char(186) << "3. Magelangan Rp.15.000" << char(186) << "3. Magelangan Rp.15.000" << char(186) << endl;
+	cout << char(186) << "4. Magelangan Rp.15.000" << char(186) << "4. Magelangan Rp.15.000" << char(186) << endl;
+	cout << char(186) << "5. Magelangan Rp.15.000" << char(186) << "5. Magelangan Rp.15.000" << char(186) << endl;
+	cout << char(186) << "6. Magelangan Rp.15.000" << char(186) << "6. Magelangan Rp.15.000" << char(186) << endl;
+
+	cout << char(211);
+	for (int x = 0; x < 23; x++) {
+		cout << char(196);
+	}
+	cout << char(208);
+	for (int x = 0; x < 23; x++) {
+		cout << char(196);
+	}
+	cout << char(189) << endl;
+
+	//Input Pesanan
+
+	//Pengkondisian
+
+	//Output Pesanan
+
+	system("pause");
+
+	return 0;
+}
