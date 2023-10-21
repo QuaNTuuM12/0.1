5635366378cee9972e9bb3100fa6c241447fd188
Achmed-Bintang <145203199+Achmed-Bintang@users.noreply.github.com>
2023-10-21 21:05:07 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,88 +1,172 @@
 #include <iostream>
 #include <iomanip>
+#include <conio.h>
+#include <string>
 
 using namespace std;
 
 int main()
 {	
-	//Daftar Menu
-	cout << setw(30) << "Daftar Menu" << endl;
+//Daftar Menu
 
-	cout << char(214);
-	for (int x = 0; x < 23; x++) {
-		cout << char(196);
+cout << setw(40) << "Daftar Menu" << endl;
+
+cout << char(214);
+for (int x = 0; x < 33; x++) {
+	cout << char(196);
+}
+cout << char(210);
+for (int x = 0; x < 33; x++) {
+	cout << char(196);
+}
+cout << char(183) << endl;
+
+cout << char(186) << setw(20) << "Makanan" << setw(14) << char(186) << setw(20) << "Minuman" << setw(14) << char(186) << endl;
+
+cout << char(204);
+for (int x = 0; x < 33; x++) {
+	cout << char(205);
+}
+cout << char(206);
+for (int x = 0; x < 33; x++) {
+	cout << char(205);
+}
+cout << char(185) << endl;
+
+cout << char(186) << setw (31) << "1. Magelangan       Rp.15.000" << setw(3) << char(186) << setw(27) << "1. Es Extrajoos       Rp.15.000" << setw(3) << char(186) << endl;
+cout << char(186) << setw (31) << "2. Nasi Goreng      Rp.15.000" << setw(3) << char(186) << setw(21) << "2. Es Teh             Rp.15.000" << setw(3) << char(186) << endl;
+cout << char(186) << setw (31) << "3. Nasi Telur       Rp.15.000" << setw(3) << char(186) << setw(23) << "3. Es Jeruk           Rp.15.000" << setw(3) << char(186) << endl;
+cout << char(186) << setw (31) << "4. Nasi Ayam        Rp.15.000" << setw(3) << char(186) << setw(19) << "4. Kopi               Rp.15.000" << setw(3) << char(186) << endl;
+	
+cout << char(211);
+for (int x = 0; x < 33; x++) {
+	cout << char(196);
+}
+cout << char(208);
+for (int x = 0; x < 33; x++) {
+	cout << char(196);
+}
+cout << char(189) << endl;
+
+//Input Pesanan
+
+string makanan, minuman, tambahan;
+float harga1, harga2, harga3;
+
+while (1) {
+	cout << "Nomor Makanan :";
+	cin >> makanan;
+
+	if (makanan == "1") {
+		cout << "Anda Membeli Magelangan Dan" << endl;
+		harga1 = 15.000;
+		break;
 	}
-	cout << char(210);
-	for (int x = 0; x < 23; x++) {
-		cout << char(196);
+	else if (makanan == "2") {
+		cout << "Anda Membeli Nasi Goreng Dan" << endl;
+		harga1 = 15.000;
+		break;
 	}
-	cout << char(183) << endl;
-
-	cout << char(186) << "\t Makanan\t" << char(186) << "\t Minuman\t" << char(186) << endl;
-
-	cout << char(204);
-	for (int x = 0; x < 23; x++) {
-		cout << char(205);
+	else if (makanan == "3") {
+		cout << "Anda Membeli Nasi Telur Dan" << endl;
+		harga1 = 15.000;
+		break;
 	}
-	cout << char(206);
-	for (int x = 0; x < 23; x++) {
-		cout << char(205);
+	else if (makanan == "4") {
+		cout << "Anda Membeli Nasi Ayam Dan" << endl;
+		harga1 = 15.000;
+		break;
 	}
-	cout << char(185) << endl;
-
-	cout << char(186) << "1. Magelangan Rp.15.000" << char(186) << "1. Es Extrajoos Rp.15.000" << char(186) << endl;
-	cout << char(186) << "2. Nasi Goreng Rp.15.000" << char(186) << "2. Es Teh Rp.15.000" << char(186) << endl;
-	cout << char(186) << "3. Nasi Telur Rp.15.000" << char(186) << "3. Es Jeruk Rp.15.000" << char(186) << endl;
-	cout << char(186) << "4. Nasi Ayam Rp.15.000" << char(186) << "4. Kopi Rp.15.000" << char(186) << endl;
-	cout << char(186) << "5. Nasi Ikan Rp.15.000" << char(186) << "5. Magelangan Rp.15.000" << char(186) << endl;
-	cout << char(186) << "6. Mie Goreng Rp.15.000" << char(186) << "6. Magelangan Rp.15.000" << char(186) << endl;
-	cout << char(186) << "7. Mie Rebus Rp.15.000" << char(186) << "6. Magelangan Rp.15.000" << char(186) << endl;
-	cout << char(186) << "8. Bubur Kacang Ijo Rp.15.000" << char(186) << "6. Magelangan Rp.15.000" << char(186) << endl;
-
-	cout << char(211);
-	for (int x = 0; x < 23; x++) {
-		cout << char(196);
+	else {
+		cout << "Menu Tidak Tersedia, Silahkan Masukan ulang" << endl;
 	}
-	cout << char(208);
-	for (int x = 0; x < 23; x++) {
-		cout << char(196);
-	}
-	cout << char(189) << endl;
-
-	//Input Pesanan
-	char makanan, minuman;
-	int harga1, harga2;
-
-	while (1) {
-
-		cout << "Pesanan Makanan :";
-		cin >> makanan;
-		cout << endl;
-
-		switch (makanan){
-		case '1':
-			cout << "Anda Memesan Magelangan" << endl;
-			break;
-		case '2':
-			cout << "anda memesan Nasi Goreng" << endl;
-			break;
-		case '3':
-			cout << "anda memesan BABI PANGGANG" << endl;
-			break;
-
-		default:
-			cout << "tidak ada dimenu" << endl;
-			
-		}
-	}
-	cout << "Pesanan Minuman :" << endl;
+	
+while (1) {
+	cout << "Nomor Minuman :";
 	cin >> minuman;
 
-	//Pengkondisian
+	if (minuman == "1") {
+		cout << "Extra Joss" << endl;
+		harga1 = 15.000;
+		break;
+	}
+	else if (minuman == "2") {
+		cout << "Es Teh" << endl;
+		harga1 = 15.000;
+		break;
+	}
+	else if (minuman == "3") {
+		cout << "Es Jeruk" << endl;
+		harga1 = 15.000;
+		break;
+	}
+	else if (minuman == "4") {
+		cout << "Kopi" << endl;
+		harga1 = 15.000;
+		break;
+	}
+	else {
+		cout << "Menu Tidak Tersedia, Silahkan Masukan ulang" << endl;
+	}
+	
+cout << char(214);
+for (int x = 0; x < 33; x++) {
+	cout << char(196);
+}
+cout << char(183) << endl;
 
-	//Output Pesanan
+cout << char(186) << setw(25) << "Tambahan Pesanan" << setw(9) << char(186) << endl;
 
-	system("pause");
+cout << char(204);
+for (int x = 0; x < 33; x++) {
+	cout << char(205);
+}
+cout << char(185) << endl;
 
-	return 0;
+cout << char(186) << setw(31) << "1. Telur             Rp.15.000" << setw(3) << char(186) << endl;
+cout << char(186) << setw(31) << "2. Cabe Level 1      Rp.15.000" << setw(3) << char(186) << endl;
+cout << char(186) << setw(31) << "3. Cabe Level 2      Rp.15.000" << setw(3) << char(186) << endl;
+cout << char(186) << setw(31) << "4. Cabe Level 3      Rp.15.000" << setw(3) << char(186) << endl;
+	
+cout << char(211);
+for (int x = 0; x < 33; x++) {
+	cout << char(196);
+}
+cout << char(189) << endl;
+
+while (1) {
+	cout << "Nomor Makanan :";
+	cin >> tambahan;
+
+	if (tambahan == "1") {
+		cout << "Penambahan Telur" << endl;
+		harga3 = 15.000;
+		break;
+	}
+	else if (tambahan == "2") {
+		cout << "Cabe Level 1" << endl;
+		harga3 = 15.000;
+		break;
+	}
+	else if (tambahan == "3") {
+		cout << "Cabe Level 2" << endl;
+		harga3 = 15.000;
+		break;
+	}
+	else if (tambahan == "4") {
+		cout << "Cabe Level 3" << endl;
+		harga3 = 15.000;
+		break;
+	}
+	else {
+		cout << "Menu Tidak Tersedia, Silahkan Masukan ulang" << endl;
+	}
+	
+//Output Pesanan
+
+cout << 
+
+system("pause");
+
+return 0;
 }
